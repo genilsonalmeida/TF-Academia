2e379648e64a292a32c1f3867c4a89d05abef39a
Jose <matiasjose547@gmail.com>
2018-11-08 11:09:50 -0300

    Add eventos relacionados a transicão de telas principal.html

---
+++ TF-Academia/Frontend/src/javascript/principal.js
@@ -0,0 +1,8 @@
+$('.cadastra-aluno').click(function(){
+    location.href = '../pages/cadastrar-aluno.html';
+});
+
+$('.lista-aluno').click(function(){
+    //location.href = '../pages/cadastrar-aluno.html';
+    alert("Pagina de listar alunos ainda não foi definida...");
+});
