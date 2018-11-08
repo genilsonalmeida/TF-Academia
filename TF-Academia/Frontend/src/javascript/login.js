fba1af90317aed2e206c9e0bdb676fdd1773648f
Jose <matiasjose547@gmail.com>
2018-11-07 23:05:34 -0300

    evento click e validação de usuario em login.js

---
+++ TF-Academia/Frontend/src/javascript/login.js
@@ -0,0 +1,10 @@
+$('.botao').click(AUTENTICACAO); //evento executa funcão -> AUTENTICACAO(event)
+
+function AUTENTICACAO(event) {
+    event.preventDefault();
+    if ($('#formGroupExampleInput').val() == 'usuario' && $('#formGroupExampleInput2').val() == '123') {
+        location.href = '../pages/principal.html';        
+    }
+    $('#formGroupExampleInput').val('');
+    $('#formGroupExampleInput2').val('');
+}
