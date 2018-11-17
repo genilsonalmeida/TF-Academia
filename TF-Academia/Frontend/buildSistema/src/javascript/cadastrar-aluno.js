e91a6ba4a80c4505a59811b901a5e1ba234f3417
Jose <matiasjose547@gmail.com>
2018-11-17 06:33:14 -0300

    Validando formulario em cadastrar-aluno.js, refatorando cadastrar-aluno.html e add jquery-mask.min.js

--- TF-Academia/Frontend/buildSistema/src/javascript/cadastrar-aluno.js
+++ TF-Academia/Frontend/buildSistema/src/javascript/cadastrar-aluno.js
@@ -1,9 +1,17 @@
+/// reponsavel pela validacão 
+$(document).ready(function () {
 
-
-/*var aluno = {
-    nome: $("#nome").val(),
-    
-}*/
+    $('#cpf').mask('000.000.000-00');
+    $('#cep').mask('00.000-000');
+    $('#numero').mask('0000');
+    $('#valorMensalidade').mask('000,00',{reverse:true});
+    $('#celular').mask('(00) 00000-0000');
+    $('#residencial').mask('(00) 0000-0000');
+    let data = new Date();
+    $('#dataMatricula').val(data.getDay()+'/'+data.getMonth()+'/'+data.getFullYear());
+     
+});
+// fim da validação
 
 function save(){
     let xhr = new XMLHttpRequest(); 
