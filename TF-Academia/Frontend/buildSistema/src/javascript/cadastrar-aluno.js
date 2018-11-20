adaf9bca757770c0ea191fae9d5fddc44b514959
Jose <matiasjose547@gmail.com>
2018-11-20 06:16:21 -0300

    adicionando demais propriedade ao objeto novo, com jquery

--- TF-Academia/Frontend/buildSistema/src/javascript/cadastrar-aluno.js
+++ TF-Academia/Frontend/buildSistema/src/javascript/cadastrar-aluno.js
@@ -1,26 +1,38 @@
+$(document).ready(function () {
 
-function save(){
-    let xhr = new XMLHttpRequest(); 
-    xhr.open('POST','http://localhost:8081/aluno');
+    $('#botao-finalizar').click(save);
 
-    xhr.setRequestHeader('Content-Type', 'application/json',true);
-    
-    
-    
-    let nome = document.getElementById('nome').value;
-    let email = document.getElementById('email').value;
-    let cpf = document.getElementById('CPF').value;
-    let cep = document.getElementById('endereco').value;
-    let novo = {
-            "nome": `${nome}`,
-            "email":`${email}`,
-            "cpf":`${cpf}`,
-            "cep":`${cep}`
-          };
-              
-    xhr.onerro = () => alert('ERRO');
-    xhr.send(JSON.stringify(novo));
-  
-   
-   
-}
+    function save(event) {
+        event.preventDefault();
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://localhost:8081/aluno');
+
+        xhr.setRequestHeader('Content-Type', 'application/json', true);
+
+        let novo = {
+            nome: $('#nome').val(),
+            dataNascimento: $('#dataNascimento').val(),
+            sexo: $('#sexo').val(),
+            cpf: $('#cpf').val(),
+            cep: $('#cep').val(),
+            endereco: $('#endereco').val(),
+            numero: $('#numero').val(),
+            cidade: $('#cidade').val(),
+            bairro: $('#bairro').val(),
+            uf: $('#uf').val(),
+            email: $('#email').val(),
+            dataMatricula: $('#dataMatricula').val(),
+            valorMensalidade: $('#valorMensalidade').val(),
+            celular: $('#celular').val(),
+            residencial: $('#residencial').val(),
+
+        };
+        
+        xhr.onerro = () => alert('ERRO');
+        xhr.send(JSON.stringify(novo));
+        
+    }
+
+
+
+});
