dd651ee4e0b6faf64569239589ae97e966a5039a
Igor Santana <igoricm1@gmail.com>
2018-11-22 17:26:42 -0200

    Adição de fonte e alteração da tela login

--- TF-Academia/Frontend/buildSistema/main.js
+++ TF-Academia/Frontend/buildSistema/main.js
@@ -18,7 +18,7 @@ if (handleSquirrelEvent(app)) {
     win.loadFile('src/pages/login.html')
     
     // Open the DevTools.
-    win.webContents.openDevTools()
+    //win.webContents.openDevTools()
   
     // Emitted when the window is closed.
     win.on('closed', () => {
