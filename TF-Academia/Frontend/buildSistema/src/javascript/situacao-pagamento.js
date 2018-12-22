8a5cec1acf34aac0fe0cd4db5cb3197db30599a2
emannuel2017 <developerredman@gmail.com>
2018-12-22 21:35:36 -0200

    alterações no frontEnd

--- TF-Academia/Frontend/buildSistema/src/javascript/situacao-pagamento.js
+++ TF-Academia/Frontend/buildSistema/src/javascript/situacao-pagamento.js
@@ -1,4 +1,4 @@
-$('#botao-cancelar').click(function(){
+$('#botao-voltar').click(function(){
     var r = confirm("Tem certeza que deseja sair da tela de pagamento??");
     if (r == true) {
         location.href = '../pages/principal.html';
