b7b34da3e2901a0eb844e7d4ff4408725bcae336
Genilson Almeida <genilsonalmeidajr@gmail.com>
2018-12-24 13:26:17 -0300

    Adicionando o ícone na aplicação.

--- TF-Academia/Frontend/buildSistema/main.js
+++ TF-Academia/Frontend/buildSistema/main.js
@@ -11,7 +11,7 @@ if (handleSquirrelEvent(app)) {
   
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({ width: 1920, height: 1200/*, frame: false*/ })
+    win = new BrowserWindow({ width: 1920, height: 1200, icon: __dirname + "/assets/icones/icon.png"  /*, frame: false*/ })
   
     // and load the index.html of the app.
     //win.loadFile('src/pages/index.html')
