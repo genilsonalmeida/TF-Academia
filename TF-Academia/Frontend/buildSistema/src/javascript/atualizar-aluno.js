61484a7482bde73d5aa499267e6d2eb36192ab82
emannuel2017 <developerredman@gmail.com>
2018-12-24 22:03:57 -0200

    adicionado atualizar-aluno.html confunções para atualizar um aluno e  a função removerAluno em listar-alunos.js

---
+++ TF-Academia/Frontend/buildSistema/src/javascript/atualizar-aluno.js
@@ -0,0 +1,74 @@
+http = new XMLHttpRequest();
+let aluno;
+$('#botao-finalizar').click(atualizarDados);
+
+function buscarAlunoPorId(){
+    let id = localStorage.getItem('idAluno');
+    http.open('GET','http://localhost:8081/aluno/'+ id);
+    http.onload = function(){
+        if(this.status == 200){
+           aluno = JSON.parse(this.responseText);
+           console.log(aluno.nome);
+           carregarDadosDoAluno(aluno);
+        }
+    }
+    
+    http.onerro = () => alert('ERRO');
+    http.send();
+}
+
+function carregarDadosDoAluno(aluno){
+    console.log(aluno);
+    $('#nome').val(aluno.nome);
+    $('#dataNascimento').val(aluno.dataDeNascimento);
+    $('#sexo').val(aluno.sexo);
+    $('#cpf').val(aluno.cpf);
+    $('#celular').val(aluno.numeroCelular);
+    $('#celular-emergencia').val(aluno.numeroCelularEmergencia);
+    $('#valorMensalidade').val(aluno.mensalidade);
+    $('#numero').val(aluno.endereco.numero);
+    $('#cep').val(aluno.endereco.cep);
+    $('#cidade').val(aluno.endereco.cidade);
+    $('#bairro').val(aluno.endereco.bairro);
+    $('#uf').val(aluno.endereco.uf);
+    $('#email').val(aluno.email);
+    $('#dataMatricula').val(aluno.dataMatricula);
+    /*
+   */
+}
+
+function atualizarDados(){
+    http.open('PUT', 'http://localhost:8081/aluno/'+ aluno.id);
+    http.setRequestHeader('Content-Type', 'application/json', true);
+    
+    http.onload = function(){
+        if(this.status == 200){
+            alert('ok');
+        }
+    }
+
+
+    let novo = {
+        nome: $('#nome').val(),
+        dataDeNascimento: $('#dataNascimento').val(),
+        sexo: $('#sexo').val(),
+        cpf: $('#cpf').val(),
+        numeroCelular:$('#celular').val(),
+        numeroCelularEmergencia:$('#celular-emergencia').val(),
+        mensalidade:$('#valorMensalidade').val(),
+        endereco: {
+            cep: $('#cep').val(),
+            numero: $('#numero').val(),
+            cidade: $('#cidade').val(),
+            bairro: $('#bairro').val(),
+            uf: $('#uf').val()
+            },
+        email: $('#email').val(),
+        dataMatricula: $('#dataMatricula').val(),
+    };
+
+    http.onerro = () => alert('ERRO');
+    http.send(JSON.stringify(novo));
+    
+}
+buscarAlunoPorId();
--- TF-Academia/Frontend/buildSistema/src/javascript/lista-de-alunos.js
+++ TF-Academia/Frontend/buildSistema/src/javascript/lista-de-alunos.js
@@ -1,14 +1,19 @@
 let recebe;
-
+let paginaAtual;
 $('#botao-voltar').click(function () {
     location.href = '../pages/principal.html';
 });
+
+$('#bnt-delete').click(function () {
+alert('ok')
+});
+
 
 function tabela(numeroPagina) {
 
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://localhost:8081/aluno/lista/'+numeroPagina);
-
+    paginaAtual = numeroPagina;
     xhr.onload = function () {
 
         if (this.status == 200) {
@@ -29,8 +34,8 @@ function atualizandoLista() {
         let cols = '';
         cols += '<th scope="row">' + i + '</th>';
         cols += '<th scope="row">' + recebe.content[i].nome + '</th>';
-        cols += '<th scope="row" onClick="editarAluno()"><img src="../../assets/icones/baseline-border_color-24px.svg"></th>';
-        cols += '<th scope="row" onClick="removerAluno()"><img src="../../assets/icones/baseline-delete-24px.svg"></th>'
+        cols += '<th scope="row" onClick="editarAluno('+i+')"><img src="../../assets/icones/baseline-border_color-24px.svg"></th>';
+        cols += '<th scope="row"  onClick="removerAluno('+i+')"><img src="../../assets/icones/baseline-delete-24px.svg"></th>'
         tr.append(cols);
         $('tbody').append(tr);
 
@@ -60,5 +65,29 @@ for(let i = 0; i < pages; i++){
  };
  }
 }
+
+function editarAluno(id) {   
+  localStorage.setItem('idAluno', recebe.content[id].id);
+  document.location = "atualizar-aluno.html"
+}
+
+function removerAluno(posicion){
+let http = new XMLHttpRequest();
+let id = recebe.content[posicion].id;
+http.open('DELETE', 'http://localhost:8081/aluno/'+ id);
+http.setRequestHeader('Content-Type', 'application/json', true);
+http.onload = function(){
+    if(this.status == 200){
+        document.getElementById("list-aluno").innerHTML = "";
+        document.getElementById("pagination-conteudo").innerHTML = ""; 
+        tabela(paginaAtual);
+    }
+}
+
+http.onerro = () => alert('ERRO');
+http.send();
+
+}
+
 tabela(0);
 selecionandoId();
