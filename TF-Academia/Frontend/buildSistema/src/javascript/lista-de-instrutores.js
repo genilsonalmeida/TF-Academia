8caa37887a9c761dbc79df58b8fe73664e66c9d9
emannuel2017 <developerredman@gmail.com>
2018-12-26 22:08:23 -0200

    adicionada função de altualizar lista de busca por nome, icone de informações sobre o instrutor, e div mostrando as informações quando o icone info é clicado

--- TF-Academia/Frontend/buildSistema/src/javascript/lista-de-instrutores.js
+++ TF-Academia/Frontend/buildSistema/src/javascript/lista-de-instrutores.js
@@ -1,15 +1,15 @@
 let recebe;
 $(document).ready(function () {
-    
+
 });
 
 
 let paginaAtual;
-   
+
 function tabela(paginadefalt) {
     paginaAtual = paginadefalt;
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:8081/instrutor/lista/'+paginadefalt);
+    xhr.open('GET', 'http://localhost:8081/instrutor/lista/' + paginadefalt);
 
     xhr.onload = function () {
 
@@ -17,7 +17,7 @@ function tabela(paginadefalt) {
             recebe = JSON.parse(this.responseText);
             console.log(recebe.totalPages);
             paginacaoDaLista(recebe.totalPages);
-            
+
             atualizandoLista();
         }
     };
@@ -46,20 +46,20 @@ $('#button-addon2').click(function () {
 
     buscarPorNome();
 });
-$("a#ajax").click(function() { // inclui todos os links com id="ajax"
-$("#ajaxContent").load($(this).attr("href")); // carrega o conteúdo da página em HREF dentro da DIV #ajaxContent (id="ajaxContent")
-return false; // remove a ação do link para navegar até a página do HREF, pois ela já foi carregada na DIV
- });
+$("a#ajax").click(function () { // inclui todos os links com id="ajax"
+    $("#ajaxContent").load($(this).attr("href")); // carrega o conteúdo da página em HREF dentro da DIV #ajaxContent (id="ajaxContent")
+    return false; // remove a ação do link para navegar até a página do HREF, pois ela já foi carregada na DIV
+});
 
- function editarAluno(id) {   
+function editarAluno(id) {
     localStorage.setItem('idInstrutor', recebe.content[id].id);
     document.location = "atualizar-instrutor.html"
-  }
+}
 
 
 
-function buscarPorNome(){
-    
+function buscarPorNome() {
+
     let nome = document.getElementById('nomeInstrutor').value;
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://localhost:8081/instrutor/find-name/' + nome);
@@ -69,14 +69,14 @@ function buscarPorNome(){
         if (this.status == 200) {
             recebe = JSON.parse(this.responseText);
             console.log(recebe);
-            if(recebe.content.length == 0){
+            if (recebe.content.length == 0) {
                 document.getElementById('alert').innerHTML = '<div class="alert alert-danger alert-dismissible">'
-                +'<strong>Não Encontrado!</strong> Este nome não Refere-se a um aluno Cadastrado.'  
-                +'</div>'
-            }else{
+                    + '<strong>Não Encontrado!</strong> Este nome não Refere-se a um aluno Cadastrado.'
+                    + '</div>'
+            } else {
                 document.getElementById('alert').innerHTML = "";
             }
-            for (var i = 0; i < recebe.content.length; i++) {              
+            for (var i = 0; i < recebe.content.length; i++) {
                 li += ' <li class="list-group-item"> Nome: ' + recebe.content[i].nome + ' || Email: ' + recebe.content[i].email + ' </li>';
                 $('ul').append(li);
             }
@@ -85,7 +85,7 @@ function buscarPorNome(){
     xhr.onerro = () => alert('ERRO');
     xhr.send();
 
-}   
+}
 
 function atualizandoLista() {
     for (var i = 0; i < recebe.content.length; i++) {
@@ -94,15 +94,16 @@ function atualizandoLista() {
         cols += '<th scope="row">' + i + '</th>';
         cols += '<th scope="row">' + recebe.content[i].nome + '</th>';
         cols += '<th scope="row">' + recebe.content[i].numeroCelular + '</th>';
-        cols += '<th scope="row" onClick="editarAluno('+i+')"><img src="../../assets/icones/baseline-border_color-24px.svg"></th>';
-        cols += '<th scope="row"  onClick="removerAluno('+i+')"><img src="../../assets/icones/baseline-delete-24px.svg"></th>'
+        cols += '<th scope="row"  onClick="carregarInfoInstrutor(' + i + ')"><img src="../../assets/icones/info.svg"></th>'
+        cols += '<th scope="row" onClick="editarAluno(' + i + ')"><img src="../../assets/icones/baseline-border_color-24px.svg"></th>';
+        cols += '<th scope="row"  onClick="removerAluno(' + i + ')"><img src="../../assets/icones/baseline-delete-24px.svg"></th>'
         tr.append(cols);
         $('tbody').append(tr);
 
     }
 }
 
-function listaDeNomesEncontrados(){
+function listaDeNomesEncontrados() {
 
     for (var i = 0; i < recebe.content.length; i++) {
         let tr = $('<tr>');
@@ -110,54 +111,65 @@ function listaDeNomesEncontrados(){
         cols += '<th scope="row">' + i + '</th>';
         cols += '<th scope="row">' + recebe.content[i].nome + '</th>';
         cols += '<th scope="row">' + recebe.content[i].numeroCelular + '</th>';
-        cols += '<th scope="row" onClick="editarAluno('+i+')"><img src="../../assets/icones/baseline-border_color-24px.svg"></th>';
-        cols += '<th scope="row"  onClick="removerAluno('+i+')"><img src="../../assets/icones/baseline-delete-24px.svg"></th>'
+        cols += '<th scope="row" onClick="editarAluno(' + i + ')"><img src="../../assets/icones/baseline-border_color-24px.svg"></th>';
+        cols += '<th scope="row"  onClick="removerAluno(' + i + ')"><img src="../../assets/icones/baseline-delete-24px.svg"></th>'
         tr.append(cols);
         $('tbody').append(tr);
-
     }
-
 }
 
-function paginacaoDaLista(qntDePaginas){
+function paginacaoDaLista(qntDePaginas) {
     let ul = $('<ul>');
     let li = '<li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>';
 
-    for(let i = 0; i < qntDePaginas; i++){
-    li += '<li class="page-item"><button type="button" class="page-link bnt">'+ i +'</button></li>';   
+    for (let i = 0; i < qntDePaginas; i++) {
+        li += '<li class="page-item"><button type="button" class="page-link bnt">' + i + '</button></li>';
     }
-   li += '<li class="page-item"><a class="page-link" href="#">Next</a></li>';
-$('ul').append(li);
+    li += '<li class="page-item"><a class="page-link" href="#">Next</a></li>';
+    $('ul').append(li);
 
-selecionandoId(qntDePaginas);
-}  
-
-function selecionandoId(pages){
-for(let i = 0; i < pages; i++){
-  console.log(i);
-    document.getElementsByClassName('bnt')[i].onclick = function () {
-    document.getElementById("list-instrutores").innerHTML = "";
-    document.getElementById("pagination-conteudo").innerHTML = ""; 
-    tabela(i);
- };
+    selecionandoId(qntDePaginas);
 }
+
+function selecionandoId(pages) {
+    for (let i = 0; i < pages; i++) {
+        console.log(i);
+        document.getElementsByClassName('bnt')[i].onclick = function () {
+            document.getElementById("list-instrutores").innerHTML = "";
+            document.getElementById("pagination-conteudo").innerHTML = "";
+            tabela(i);
+        };
+    }
 }
 
 function removerAluno(posicao) {
     let http = new XMLHttpRequest();
     let id = recebe.content[posicao].id;
-    http.open('DELETE', 'http://localhost:8081/instrutor/'+ id);
+    http.open('DELETE', 'http://localhost:8081/instrutor/' + id);
     http.setRequestHeader('Content-Type', 'application/json', true);
-    http.onload = function(){
-        if(this.status == 200){
+    http.onload = function () {
+        if (this.status == 200) {
             document.getElementById("list-instrutores").innerHTML = "";
-            document.getElementById("pagination-conteudo").innerHTML = ""; 
+            document.getElementById("pagination-conteudo").innerHTML = "";
             tabela(paginaAtual);
         }
     }
-    
+
     http.onerro = () => alert('ERRO');
-    http.send();    
+    http.send();
 }
+
+function carregarInfoInstrutor(instrutor) {
+    console.log('buscol');
+    let div = document.getElementById('info-instrutor');
+
+    div.innerHTML += '<div class="media" >';
+    div.innerHTML += '<img src="img_avatar1.png" class="align-self-start mr-3" style="width:60px">';
+    div.innerHTML += '<div class="media-body">';
+    div.innerHTML += '<h4>' + recebe.content[instrutor].nome + '</h4>';
+    div.innerHTML += '<p>Número:' + recebe.content[instrutor].numeroCelular + ': Número de emergêmcia:' + recebe.content[instrutor].numeroCelularEmergencia + '</p>';
+    div.innerHTML += '</div></div>';
+}
+
 tabela(0);
 selecionandoId();
