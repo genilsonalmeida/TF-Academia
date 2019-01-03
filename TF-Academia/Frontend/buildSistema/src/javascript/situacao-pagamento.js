9cd89aa644be9ee2a978c60bbdb0e4155b7c057d
Genilson Almeida <genilsonalmeidajr@gmail.com>
2019-01-02 21:24:16 -0300

    Confirmação para excluir pagamento. Adição de mais um número em telefone de emergência.

--- TF-Academia/Frontend/buildSistema/src/javascript/situacao-pagamento.js
+++ TF-Academia/Frontend/buildSistema/src/javascript/situacao-pagamento.js
@@ -2,7 +2,7 @@
 let registroId = localStorage.getItem('registroId');
 let http = new XMLHttpRequest();
 
-$('#botao-voltar').click(function(){
+$('#botao-voltar').click(function () {
     var r = confirm("Tem certeza que deseja sair da tela de pagamento??");
     if (r == true) {
         location.href = '../pages/principal.html';
@@ -11,7 +11,7 @@ $('#botao-voltar').click(function(){
 
 jQuery(window).load(function () {
     $(".loader").delay(500).fadeOut("slow"); //retire o delay quando for copiar!
-  $("#tudo_page").toggle("fast");
+    $("#tudo_page").toggle("fast");
 });
 
 
@@ -25,7 +25,7 @@ function tabela(numeroPagina) {
         if (this.status == 200) {
             recebe = JSON.parse(this.responseText);
             console.log(recebe);
-           // paginacaoDaLista(recebe.totalPages);
+            // paginacaoDaLista(recebe.totalPages);
             atualizandoLista();
         }
     };
@@ -34,14 +34,19 @@ function tabela(numeroPagina) {
 
 }
 
+function newDate(){
+    console.log("Entrei!");
+}
+
 function atualizandoLista() {
     document.getElementById('list-aluno').innerHTML = "";
     for (var i = 0; i < recebe.pagamentos.length; i++) {
         let tr = $('<tr>');
         let cols = '';
+        var data = newDate(recebe.pagamentos[i].dataDoPagamento);
         cols += '<th scope="row">' + (i + 1) + '</th>';
         cols += '<th scope="row">' + recebe.pagamentos[i].dataDoPagamento + '</th>';
-        cols += '<th scope="row">' + recebe.pagamentos[i].valor + '</th>'
+        cols += '<th scope="row"> R$ ' + recebe.pagamentos[i].valor + ',00 </th>'
         cols += '<th scope="row">' + recebe.pagamentos[i].descricaoDoPagamento + '</th>';
         cols += '<th scope="row"  onClick="removerPagamento(' + i + ')"><img src="../../assets/icones/baseline-delete-24px.svg"></th>'
         tr.append(cols);
@@ -78,30 +83,30 @@ function buscarPorNomeNumeroCelular() {
     xhr.send();
 }
 
-function adicionarNomeDoAlunoADiv(){
+function adicionarNomeDoAlunoADiv() {
     document.getElementById('nome-aluno').innerHTML = localStorage.getItem('alunoNome');
 }
 
-function removerPagamento(posicao){
+function removerPagamento(posicao) {
 
-    //    var r = confirm("Tem certeza que deseja excluir o aluno?");
-     ///   if (r == true) {
-            let http = new XMLHttpRequest();
-            http.open('DELETE', 'http://localhost:8081/registroPagamento/'+ registroId +'/deletePagamento');
-            http.setRequestHeader('Content-Type', 'application/json', true);
-            http.onload = function () {
-                if (this.status == 200) {
-                    tabela(0);
-                }
+    var r = confirm("Tem certeza que deseja excluir o pagamento?");
+    if (r == true) {
+        let http = new XMLHttpRequest();
+        http.open('DELETE', 'http://localhost:8081/registroPagamento/' + registroId + '/deletePagamento');
+        http.setRequestHeader('Content-Type', 'application/json', true);
+        http.onload = function () {
+            if (this.status == 200) {
+                tabela(0);
             }
-            let pagamento = {
-                'id' : 	recebe.pagamentos[posicao].id
-            };
-    
+        }
+        let pagamento = {
+            'id': recebe.pagamentos[posicao].id
+        };
+
         http.onerro = () => alert('ERRO');
         http.send(JSON.stringify(pagamento));
- }
-    
-//}
+    }
+
+}
 tabela(0);
 adicionarNomeDoAlunoADiv();
